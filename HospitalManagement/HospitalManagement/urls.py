7edc51a7d03cbc618600e9b9c959f4a68580419a
Tama Shil <51587530+Tama-Shil@users.noreply.github.com>
2024-02-11 21:34:03 +0600

    Update urls.py

--- HospitalManagement/HospitalManagement/urls.py
+++ HospitalManagement/HospitalManagement/urls.py
@@ -1,9 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("HospitalManagementSystem.urls")),
 ]
 
