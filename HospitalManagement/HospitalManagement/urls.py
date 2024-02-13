b01760e12ba25c2d0531cb4e875cc3323dcabaf1
Tama-Shil <jucse28.342@gmail.com>
2024-02-13 19:52:34 +0600

    pulled changes

--- HospitalManagement/HospitalManagement/urls.py
+++ HospitalManagement/HospitalManagement/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('',views.home, name='home')
+    path('home/',views.home, name='home'),
+    path('',include("App_Payment.urls")),
 ]
 
