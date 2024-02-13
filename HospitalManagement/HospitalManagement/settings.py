7e9031a9206f5a22c612be4c90020f2ad3e6a1f6
tiri39 <etisadiaafrin@gmail.com>
2024-02-13 20:29:33 +0600

    updated settings

--- HospitalManagement/HospitalManagement/settings.py
+++ HospitalManagement/HospitalManagement/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'HospitalManagement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
