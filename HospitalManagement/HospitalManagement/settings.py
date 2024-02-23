a1a28ae05908044fe4453580b3336522950dfe7b
Pritam Saha <jucse28.375@gmail.com>
2024-02-23 12:36:15 +0600

    BuyMedicine app created

--- HospitalManagement/HospitalManagement/settings.py
+++ HospitalManagement/HospitalManagement/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App_AllocateBed',
+    'App_BuyMedicine',
 
 ]
 
--- HospitalManagement/HospitalManagement/urls.py
+++ HospitalManagement/HospitalManagement/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("App_AllocateBed.urls")),
+    path('allocate/',include("App_AllocateBed.urls")),
 ]
 
