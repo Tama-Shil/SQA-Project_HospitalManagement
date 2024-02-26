b486bc74aa69fe9f12ad24fbd466c0ec1731395b
epiprokash <prokash.stu2018@juniv.edu>
2024-02-26 12:42:07 +0600

    Applied TDD and fixed some issues

--- HospitalManagement/make_appointment/admin.py
+++ HospitalManagement/make_appointment/admin.py
@@ -1,3 +1,11 @@
+# make_appointment/admin.py
 from django.contrib import admin
+from .models import Patient, Appointment
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_number')
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'date', 'time')
